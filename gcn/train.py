d12b6140142ce522293d93b28a15a2914c1fc173
Ines Oliveira e Silva <inesoliveiraesilva@gmail.com>
2021-03-27 11:20:58 +0000

    started gcn

--- gcn/train.py
+++ gcn/train.py
@@ -1,51 +1,133 @@
-import torch
+import tensorflow as tf
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import torch.nn as nn
-import matplotlib.pyplot as plt
-from model import Model
+from model import KGCN
 
-if __name__ == '__main__':
-    df = pd.read_pickle('C:\\Users\\ineso\\FEUP-3ano\\gulbenkian-ai\\gcn\\user_cat_bool_df.pickle')
-    df = df.astype({'user_id':'category'})
-    df = df.sample(frac=0.1, random_state=1)
-    categorical_columns = list(df.columns)
-    categorical_columns.remove('stars')
+def train(args, data, show_loss, show_topk):
+    n_user, n_item, n_entity, n_relation = data[0], data[1], data[2], data[3]
+    train_data, eval_data, test_data = data[4], data[5], data[6]
+    adj_entity, adj_relation = data[7], data[8]
 
-    categorical_data = np.stack([df[c].cat.codes.values for c in categorical_columns], 1)
-    categorical_data = torch.tensor(categorical_data, dtype=torch.int64)
+    model = KGCN(args, n_user, n_entity, n_relation, adj_entity, adj_relation)
 
-    outputs = torch.tensor(df['stars'].values).flatten()
+    # top-K evaluation settings
+    user_list, train_record, test_record, item_set, k_list = topk_settings(show_topk, train_data, test_data, n_item)
 
-    train_data, test_data, train_output, test_output = train_test_split(categorical_data, outputs, test_size=0.2, random_state=42)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    categorical_column_sizes = [len(df[column].cat.categories) for column in categorical_columns]
-    categorical_embedding_sizes = [(col_size, min(50, (col_size+1)//2)) for col_size in categorical_column_sizes]
+        for step in range(args.n_epochs):
+            # training
+            np.random.shuffle(train_data)
+            start = 0
+            # skip the last incomplete minibatch if its size < batch size
+            while start + args.batch_size <= train_data.shape[0]:
+                _, loss = model.train(sess, get_feed_dict(model, train_data, start, start + args.batch_size))
+                start += args.batch_size
+                if show_loss:
+                    print(start, loss)
 
-    model = Model(categorical_embedding_sizes, 5, [200,100,50], p=0.4)
+            # CTR evaluation
+            train_auc, train_f1 = ctr_eval(sess, model, train_data, args.batch_size)
+            eval_auc, eval_f1 = ctr_eval(sess, model, eval_data, args.batch_size)
+            test_auc, test_f1 = ctr_eval(sess, model, test_data, args.batch_size)
 
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+            print('epoch %d    train auc: %.4f  f1: %.4f    eval auc: %.4f  f1: %.4f    test auc: %.4f  f1: %.4f'
+                  % (step, train_auc, train_f1, eval_auc, eval_f1, test_auc, test_f1))
 
-    epochs = 300
-    aggregated_losses = []
+            # top-K evaluation
+            if show_topk:
+                precision, recall = topk_eval(
+                    sess, model, user_list, train_record, test_record, item_set, k_list, args.batch_size)
+                print('precision: ', end='')
+                for i in precision:
+                    print('%.4f\t' % i, end='')
+                print()
+                print('recall: ', end='')
+                for i in recall:
+                    print('%.4f\t' % i, end='')
+                print('\n')
 
-    for i in range(epochs):
-        i += 1
-        y_pred = model(train_data)
-        single_loss = loss_function(y_pred, train_output-1)
-        aggregated_losses.append(single_loss)
 
-        if i%25 == 1:
-            print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+def topk_settings(show_topk, train_data, test_data, n_item):
+    if show_topk:
+        user_num = 100
+        k_list = [1, 2, 5, 10, 20, 50, 100]
+        train_record = get_user_record(train_data, True)
+        test_record = get_user_record(test_data, False)
+        user_list = list(set(train_record.keys()) & set(test_record.keys()))
+        if len(user_list) > user_num:
+            user_list = np.random.choice(user_list, size=user_num, replace=False)
+        item_set = set(list(range(n_item)))
+        return user_list, train_record, test_record, item_set, k_list
+    else:
+        return [None] * 5
 
-        optimizer.zero_grad()
-        single_loss.backward()
-        optimizer.step()
 
-    print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+def get_feed_dict(model, data, start, end):
+    feed_dict = {model.user_indices: data[start:end, 0],
+                 model.item_indices: data[start:end, 1],
+                 model.labels: data[start:end, 2]}
+    return feed_dict
 
-    plt.plot(range(epochs), aggregated_losses)
-    plt.ylabel('Loss')
-    plt.xlabel('epoch')
+
+def ctr_eval(sess, model, data, batch_size):
+    start = 0
+    auc_list = []
+    f1_list = []
+    while start + batch_size <= data.shape[0]:
+        auc, f1 = model.eval(sess, get_feed_dict(model, data, start, start + batch_size))
+        auc_list.append(auc)
+        f1_list.append(f1)
+        start += batch_size
+    return float(np.mean(auc_list)), float(np.mean(f1_list))
+
+
+def topk_eval(sess, model, user_list, train_record, test_record, item_set, k_list, batch_size):
+    precision_list = {k: [] for k in k_list}
+    recall_list = {k: [] for k in k_list}
+
+    for user in user_list:
+        test_item_list = list(item_set - train_record[user])
+        item_score_map = dict()
+        start = 0
+        while start + batch_size <= len(test_item_list):
+            items, scores = model.get_scores(sess, {model.user_indices: [user] * batch_size,
+                                                    model.item_indices: test_item_list[start:start + batch_size]})
+            for item, score in zip(items, scores):
+                item_score_map[item] = score
+            start += batch_size
+
+        # padding the last incomplete minibatch if exists
+        if start < len(test_item_list):
+            items, scores = model.get_scores(
+                sess, {model.user_indices: [user] * batch_size,
+                       model.item_indices: test_item_list[start:] + [test_item_list[-1]] * (
+                               batch_size - len(test_item_list) + start)})
+            for item, score in zip(items, scores):
+                item_score_map[item] = score
+
+        item_score_pair_sorted = sorted(item_score_map.items(), key=lambda x: x[1], reverse=True)
+        item_sorted = [i[0] for i in item_score_pair_sorted]
+
+        for k in k_list:
+            hit_num = len(set(item_sorted[:k]) & test_record[user])
+            precision_list[k].append(hit_num / k)
+            recall_list[k].append(hit_num / len(test_record[user]))
+
+    precision = [np.mean(precision_list[k]) for k in k_list]
+    recall = [np.mean(recall_list[k]) for k in k_list]
+
+    return precision, recall
+
+
+def get_user_record(data, is_train):
+    user_history_dict = dict()
+    for interaction in data:
+        user = interaction[0]
+        item = interaction[1]
+        label = interaction[2]
+        if is_train or label == 1:
+            if user not in user_history_dict:
+                user_history_dict[user] = set()
+            user_history_dict[user].add(item)
+    return user_history_dict
